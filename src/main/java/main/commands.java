beda3a538ad26fd0bd359c1d7053b6436845063f
wyatt_was <127136122+wyattwas@users.noreply.github.com>
2023-07-04 15:40:04 +0000

    /profile command

--- src/main/java/main/commands.java
+++ src/main/java/main/commands.java
@@ -1,9 +1,19 @@
 package main;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.entities.User.Profile;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.annotation.Nonnull;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class commands extends ListenerAdapter {
 
@@ -11,5 +21,29 @@ public class commands extends ListenerAdapter {
     public void onSlashCommandInteraction(@Nonnull SlashCommandInteractionEvent event) {
         String command = event.getName();
 
+        if (command.equals("profile")) {
+            ObjectMapper mapper = new ObjectMapper();
+            Map<Integer, Profile> map = new HashMap<Integer, Profile>();
+            EmbedBuilder emb = new EmbedBuilder();
+            String user = event.getOption("minecraft-name").toString();
+            try {
+                map = mapper.readValue(new File("http://minecraft.data.lxbs.online/profile/profile.json"),
+                        new TypeReference<Map<Integer, Profile>>() {
+                        });
+                for (Profile value : map.values()) {
+                    if (value.getUserNameString() == user) {
+                        emb.setTitle(user)
+                                .setImage(user)
+                                .setColor(0x4d8a5d)
+                                .addField("Network Level", value.getLevel(), true)
+                                .addField("Status", value.getOnlineStatus(), true);
+                    } else {
+                        event.reply("There is no player with that name").queue();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/main/contextMenu.java
+++ src/main/java/main/contextMenu.java
@@ -1,10 +1,18 @@
 package main;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Nonnull;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.User.Profile;
 import net.dv8tion.jda.api.events.interaction.command.UserContextInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -12,6 +20,29 @@ public class contextMenu extends ListenerAdapter {
 
     @Override
     public void onUserContextInteraction(@Nonnull UserContextInteractionEvent event) {
-
+        if (event.getName().equals("profile")) {
+            ObjectMapper mapper = new ObjectMapper();
+            Map<Integer, Profile> map = new HashMap<Integer, Profile>();
+            EmbedBuilder emb = new EmbedBuilder();
+            String user = event.getTarget().toString();
+            try {
+                map = mapper.readValue(new File("http://minecraft.data.lxbs.online/profile/profile.json"),
+                        new TypeReference<Map<Integer, Profile>>() {
+                        });
+                for (Profile value : map.values()) {
+                    if (value.getUserNameString() == user) {
+                        emb.setTitle(user)
+                                .setImage(user)
+                                .setColor(0x4d8a5d)
+                                .addField("Network Level", value.getLevel(), true)
+                                .addField("Status", value.getOnlineStatus(), true);
+                    } else {
+                        event.reply("There is no player with that name").queue();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
